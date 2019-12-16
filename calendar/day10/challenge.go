cbf3b26c71099434bcf18d120b8bfc0b7b1836dc
Gwenael Vassilieff <gwenael@swapcard.com>
2019-12-16 17:50:54 +0100

    add day10 part2

--- calendar/day10/challenge.go
+++ calendar/day10/challenge.go
@@ -2,20 +2,39 @@ package day10
 
 import (
 	"bufio"
-	"container/list"
-	"errors"
+	"fmt"
 	"io"
+	"math"
+	"sort"
 	"strconv"
 )
+
+func abs(n int) int {
+	if n < 0 {
+		return n * -1
+	}
+	return n
+}
+
+type asteroids []point
 
 type point struct {
 	x, y int
 }
 
-type asteroid point
-
 type Day10 struct {
-	asteroidBelt *list.List
+	asteroids asteroids
+}
+
+func (p point) distance(origin point) int {
+	return abs(p.y-origin.y) + abs(p.x-origin.x)
+}
+
+func (p point) angle(origin point) float64 {
+	theta := math.Atan2(float64(p.y-origin.y), float64(p.x-origin.x))
+	angle := (theta / (math.Pi / 180)) + 90
+	angle = (angle) + math.Ceil(-angle/360)*360
+	return angle
 }
 
 func (d Day10) InputPath() string {
@@ -23,13 +42,12 @@ func (d Day10) InputPath() string {
 }
 
 func (d *Day10) Prepare(input io.Reader) error {
-	d.asteroidBelt = list.New()
 	scanner := bufio.NewScanner(input)
 	for y := 0; scanner.Scan(); y++ {
 		belt := scanner.Bytes()
 		for x, c := range belt {
 			if c == '#' {
-				d.asteroidBelt.PushBack(asteroid{x, y})
+				d.asteroids = append(d.asteroids, point{x, y})
 			}
 		}
 	}
@@ -39,65 +57,85 @@ func (d *Day10) Prepare(input io.Reader) error {
 	return nil
 }
 
-func abs(n int) int {
-	if n < 0 {
-		return n * -1
-	}
-	return n
-}
-
-func findBestPosition(station asteroid, asteroidBeltSrc *list.List) int {
-	asteroidBelt := list.New()
-	asteroidBelt.PushBackList(asteroidBeltSrc)
-	front := asteroidBelt.Front()
-	for front != nil {
-		asteroid1 := front.Value.(asteroid)
-		a1 := asteroid{asteroid1.x - station.x, asteroid1.y - station.y}
-		for e := front.Next(); e != nil; {
-			a2 := e.Value.(asteroid)
-			a2 = asteroid{a2.x - station.x, a2.y - station.y}
-			//	fmt.Printf("%v %v\t(%v) (%v) =>> %v\n", a1, a2, (a1.x^a2.x) >= 0 || (a1.x*a2.x) == 0, (a1.y^a2.y) >= 0 || (a1.y*a2.y) == 0, a1.x*a2.y-a1.y*a2.x == 0)
-			if ((a1.x^a2.x) >= 0 || (a1.x*a2.x) == 0) && ((a1.y^a2.y) >= 0 || (a1.y*a2.y) == 0) && a1.x*a2.y-a1.y*a2.x == 0 {
-				if abs(a1.x)+abs(a1.y) < abs(a2.x)+abs(a2.y) {
-					tmp := e.Next()
-					asteroidBelt.Remove(e)
-					e = tmp
-				} else {
-					goto skip
-				}
-			} else {
-				e = e.Next()
+func (d *Day10) Part1() (string, error) {
+	bestPosition := point{}
+	mostSeen := 0
+	for i, candidate := range d.asteroids {
+		seenMap := make(map[float64]point)
+		for n, asteroid := range d.asteroids {
+			if i != n {
+				seenMap[asteroid.angle(candidate)] = asteroid
 			}
 		}
-		front = front.Next()
-		continue
-	skip:
-		tmp := front.Next()
-		asteroidBelt.Remove(front)
-		front = tmp
-	}
-	return asteroidBelt.Len()
-}
-
-func (d *Day10) Part1() (string, error) {
-	best := 0
-	for e := d.asteroidBelt.Front(); e != nil; e = e.Next() {
-		asteroid := e.Value.(asteroid)
-		tmp := e.Prev()
-		d.asteroidBelt.Remove(e)
-		count := findBestPosition(asteroid, d.asteroidBelt)
-		if tmp != nil {
-			e = d.asteroidBelt.InsertAfter(asteroid, tmp)
-		} else {
-			e = d.asteroidBelt.PushFront(asteroid)
-		}
-		if count > best {
-			best = count
+		if mostSeen < len(seenMap) {
+			mostSeen = len(seenMap)
+			bestPosition = candidate
 		}
 	}
-	return strconv.Itoa(best), errors.New("todo")
+	// fmt.Printf("best station position %+v\n", bestPosition)
+	return fmt.Sprintf("%d (x:%d, y:%d)", mostSeen, bestPosition.x, bestPosition.y), nil
 }
 
+type VisionVector struct {
+	angle     float64
+	asteroids asteroids
+}
+type VisionVectors []VisionVector
+
+func (v VisionVectors) Len() int {
+	return len(v)
+}
+
+func (v VisionVectors) Less(i, j int) bool {
+	return v[j].angle > v[i].angle
+}
+
+func (v VisionVectors) Swap(i, j int) {
+	v[i], v[j] = v[j], v[i]
+}
+
+func (v asteroids) Len() int {
+	return len(v)
+}
+
+func (v asteroids) Swap(i, j int) {
+	v[i], v[j] = v[j], v[i]
+}
+
+var station = point{11, 13}
+
+func (v asteroids) Less(i, j int) bool {
+	return v[j].distance(station) < v[i].distance(station)
+}
 func (d *Day10) Part2() (string, error) {
-	return "", errors.New("todo")
+	seenMap := make(map[float64]asteroids)
+	for _, asteroid := range d.asteroids {
+		if asteroid.x == station.x && asteroid.y == station.y {
+			continue
+		}
+		angle := asteroid.angle(station)
+		seenMap[angle] = append(seenMap[angle], asteroid)
+	}
+	seenArray := make(VisionVectors, 0, len(seenMap))
+	for k, v := range seenMap {
+		vector := VisionVector{k, v}
+		sort.Sort(vector.asteroids)
+		seenArray = append(seenArray, vector)
+	}
+	sort.Sort(seenArray)
+	for i, destroyed := 0, 0; ; {
+		vector := seenArray[i]
+		if len(vector.asteroids) > 0 {
+			destroyed++
+			if destroyed == 200 {
+				asteroid := vector.asteroids[len(vector.asteroids)-1]
+				return strconv.Itoa(asteroid.x*100 + asteroid.y), nil
+			}
+			seenArray[i].asteroids = vector.asteroids[:len(vector.asteroids)-1]
+		}
+		i++
+		if i == len(seenArray) {
+			i = 0
+		}
+	}
 }
