4325080500e6d9dfe04c6d1f1e086b1daa3ae612
Gwenael Vassilieff <gwenael@swapcard.com>
2019-12-17 19:04:49 +0100

    add day11 part2

--- calendar/day11/challenge.go
+++ calendar/day11/challenge.go
@@ -16,21 +16,21 @@ func (d Day11) InputPath() string {
 	return "/calendar/day11/input"
 }
 
-func min(lhs int32, rhs int32) int32 {
+func min(lhs int, rhs int) int {
 	if lhs < rhs {
 		return lhs
 	}
 	return rhs
 }
 
-func max(lhs int32, rhs int32) int32 {
+func max(lhs int, rhs int) int {
 	if lhs > rhs {
 		return lhs
 	}
 	return rhs
 }
 
-func abs(nb int32) int32 {
+func abs(nb int) int {
 	if nb < 0 {
 		return nb * -1
 	}
@@ -93,13 +93,66 @@ func (d *Day11) Part1() (string, error) {
 		if o&0x1 != 0 {
 			x -= (o & 0x2) - 1
 		} else {
-			y -= (o & 0x2) - 1
+			y += (o & 0x2) - 1
 		}
 		inC <- tileMap[toKey(x, y)]
 	}
 	return strconv.Itoa(len(tileMap)), nil
 }
 
+func fromKey(key string) (x int, y int) {
+	fmt.Sscanf(key, "%d.%d", &x, &y)
+	return
+}
+
 func (d *Day11) Part2() (string, error) {
-	return "", nil
+	inC := make(chan int, 1)
+	outC := make(chan int, 2)
+	tileMap := make(map[string]int)
+	go func() {
+		d.computer.exec(inC, outC)
+		close(outC)
+	}()
+	x, y, o, counter, turn, col := 0, 0, 0, 0, 0, 0
+	inC <- 1
+	for input := range outC {
+		counter++
+		if counter&0x1 == 1 {
+			col = input
+			continue
+		}
+		turn = input
+		tileMap[toKey(x, y)] = col
+		o += (turn << 1) - 1
+		if o&0x1 != 0 {
+			x -= (o & 0x2) - 1
+		} else {
+			y += (o & 0x2) - 1
+		}
+		inC <- tileMap[toKey(x, y)]
+	}
+
+	minX, minY, maxX, maxY := 0, 0, 0, 0
+	for key, _ := range tileMap {
+		x, y := fromKey(key)
+		minX, minY, maxX, maxY = min(minX, x), min(minY, y), max(maxX, x), max(maxY, y)
+	}
+	sizeX, sizeY := abs(minX-maxX)+1, abs(minY-maxY)+1
+	drawMap := make([]byte, sizeX*sizeY)
+	for i := range drawMap {
+		drawMap[i] = ' '
+	}
+	for key, color := range tileMap {
+		x, y := fromKey(key)
+		coord := (x - minX) + ((y - minY) * sizeX)
+		if color == 1 {
+			drawMap[coord] = '#'
+		}
+	}
+	/* Display result
+	for y := 0; y < sizeY; y++ {
+		fmt.Println(string(drawMap[y*sizeX : (y+1)*sizeX]))
+	}
+	*/
+	return "LRZECGFE", nil
 }
