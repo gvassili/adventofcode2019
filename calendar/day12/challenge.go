498102c2fc38e3e778c55dbbcf56d28ac5af3d83
Gwenael Vassilieff <gwenael@swapcard.com>
2019-12-22 22:38:46 +0100

    add day12 part2

--- calendar/day12/challenge.go
+++ calendar/day12/challenge.go
@@ -66,29 +66,82 @@ func (b *body) move() {
 	b.x, b.y, b.z = b.x+b.vx, b.y+b.vy, b.z+b.vz
 }
 
+func boolToInt(cond bool) int {
+	if cond == false {
+		return 0
+	}
+	return 1
+}
+
+func (b *body) alignment(other body) int {
+	return boolToInt(b.x == other.x) |
+		(boolToInt(b.y == other.y) << 1) |
+		(boolToInt(b.z == other.z) << 2)
+}
+
 func (b *body) energy() int {
 	return (abs(b.x) + abs(b.y) + abs(b.z)) * (abs(b.vx) + abs(b.vy) + abs(b.vz))
 }
 
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+
+	return a
+}
+
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
 func (d *Day12) Part1() (string, error) {
+	system := make([]body, len(d.system))
+	copy(system, d.system)
 	for step := 0; step < 1000; step++ {
-		for i := 0; i < len(d.system)-1; i++ {
-			for n := i + 1; n < len(d.system); n++ {
-				d.system[i].applyGravity(d.system[n])
-				d.system[n].applyGravity(d.system[i])
+		for i := 0; i < len(system)-1; i++ {
+			for n := i + 1; n < len(system); n++ {
+				system[i].applyGravity(system[n])
+				system[n].applyGravity(system[i])
 			}
 		}
-		for i := range d.system {
-			d.system[i].move()
+		for i := range system {
+			system[i].move()
 		}
 	}
 	sumEnergy := 0
-	for _, body := range d.system {
+	for _, body := range system {
 		sumEnergy += body.energy()
 	}
 	return strconv.Itoa(sumEnergy), nil
 }
 
 func (d *Day12) Part2() (string, error) {
-	return "", nil
+	system := make([]body, len(d.system))
+	ax, ay, az, afound := 0, 0, 0, 0
+	copy(system, d.system)
+	for step := 2; afound != 0x7; step++ {
+		for i := 0; i < len(system)-1; i++ {
+			for n := i + 1; n < len(system); n++ {
+				system[i].applyGravity(system[n])
+				system[n].applyGravity(system[i])
+			}
+		}
+		alignment := 0x7
+		for i := range system {
+			system[i].move()
+			alignment &= system[i].alignment(d.system[i])
+		}
+		if ax == 0 && (alignment&1) != 0 {
+			ax = step
+		}
+		if ay == 0 && (alignment&(1<<1)) != 0 {
+			ay = step
+		}
+		if az == 0 && (alignment&(1<<2)) != 0 {
+			az = step
+		}
+		afound |= alignment
+	}
+	return strconv.Itoa(lcm(lcm(ax, ay), az)), nil
 }
